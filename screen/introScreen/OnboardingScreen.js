e1d16dcc90ca60fe104432c2b3e771e6bac50c37
Pheemaphat <Pheemaphat@gmail.com>
2023-02-27 22:22:50 +0700

    commit

--- screen/introScreen/OnboardingScreen.js
+++ screen/introScreen/OnboardingScreen.js
@@ -1,5 +1,12 @@
 import { useEffect } from "react";
-import { View, Text, Image } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  Button,
+  TouchableOpacity,
+  TouchableHighlight,
+} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import wash_machine from "../../assets/washing-machine.png";
@@ -10,12 +17,80 @@ const OnboardingScreen = ({ navigation, props }) => {
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Image source={wash_machine} style={image} resizeMode="contain" />
-      <Text style={text}>Suck&Reed</Text>
+      <View
+        style={{
+          width: 500,
+          height: 400,
+          borderBottomStartRadius: 500 / 2,
+          borderBottomEndRadius: 500 / 2,
+          position: "absolute",
+          backgroundColor: "#4691FB",
+        }}
+      />
+      <Image
+        source={wash_machine}
+        style={{ width: "60%", height: "30%", marginTop: "25%" }}
+        resizeMode="contain"
+      />
+      <View
+        style={{
+          marginTop: "25%",
+          width: "100%",
+          height: "100%",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 42,
+            // color: "#0000",
+            fontFamily: "Montserrat",
+          }}
+        >
+          Suck&Reed
+        </Text>
+        <TouchableOpacity
+          style={{
+            marginTop: "15%",
+            borderRadius: 10,
+            width: "90%",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#4691FB",
+            paddingVertical: 20,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 18,
+              color: "#ffff",
+            }}
+          >
+            สร้างบัญชีใหม่
+          </Text>
+        </TouchableOpacity>
+
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            alignItems: "center",
+            marginVertical: 100,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 14,
+              //   color: "#ffff",
+            }}
+          >
+            มีบัญชีอยู่แล้ว? เข้าสู่ระบบ
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
