f076e13da1785d0029dbe187afd37a12189679d3
Pheemaphat <Pheemaphat@gmail.com>
2023-03-27 20:25:46 +0700

    update

---
+++ src/api/GetApi.js
@@ -0,0 +1,34 @@
+// const BASEURL =
+//   Platform.OS === "android"
+//     ? "http://10.0.2.2/CS-PROJECT-BACKEND"
+//     : "http://localhost/CS-PROJECT-BACKEND";
+
+// SelectShopScreen line 66-74
+// SelectServiceScreen line 82
+
+const BASEURL = "https://0ef0-223-24-161-140.ap.ngrok.io"
+
+export const API = {
+  url: BASEURL + "/CS-PROJECT-BACKEND",
+  urlCustomerImage: BASEURL + "/CS-PROJECT-BACKEND/customerAssets/",
+  urlLaundryImage: BASEURL + "/CS-PROJECT-BACKEND/laundryAssets/",
+  urlRiderImage: BASEURL + "/CS-PROJECT-BACKEND/riderAssets/",
+};
+
+const useFetch = async (method, body, path) => {
+
+  var requestOptions = {
+    method: method,
+    body: body,
+    redirect: "follow",
+  };
+
+  return await fetch(API.url + path, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => console.log("error", error));
+};
+
+export default { useFetch };
